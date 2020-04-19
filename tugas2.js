5289cfd3a7c050105d43f4627d94c6ff9f7b328c
SongKro <songkro48@gmail.com>
2020-04-19 07:11:42 +0700

    done

--- tugas2.js
+++ tugas2.js
@@ -1,4 +1,9 @@
 console.log('LOOPING PERTAMA')
-for(let loop = 0; loop <= 20; loop += 1){
+for(let loop = 1; loop <= 20; loop += 1){
     console.log(loop + ' - I love coding')
+}
+
+console.log('LOOPING KEDUA')
+for(let loop2 = 20; loop2 >= 1; loop2 -= 1){
+    console.log(loop2 + ' - I will become fullstack developer')
 }
