e9db619f99546b9ae19dbbdc9c5423707dd72f47
SongKro <songkro48@gmail.com>
2020-04-19 07:20:15 +0700

    done

--- tugas1.js
+++ tugas1.js
@@ -1,1 +1,13 @@
-LOOPING PERTAMA
+let loop = 2    
+console.log('LOOPING PERTAMA')
+while(loop <= 20){
+    console.log(loop + ' - I love coding')
+    loop += 2
+}
+
+let loop2 = 20
+console.log('LOOPING KEDUA')
+while(loop2 > 0){
+    console.log(loop2 +' - I will become fullstack developer')
+    loop2 -= 2
+}
