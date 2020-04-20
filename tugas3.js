2fba6e204fec5d94332404f46b0839d0731589db
SongKro <songkro48@gmail.com>
2020-04-20 12:51:10 +0700

    done

---
+++ tugas3.js
@@ -0,0 +1,33 @@
+// for( let angka = 1; angka <=100; angka++){
+//     if (angka % 2 === 0 ){
+//     console.log(angka + ' GENAP')
+//          } else {
+//     console.log(angka +' GANJIL')
+//          }
+    
+//      }
+
+for(let angka2 = 1; angka2 <= 100; angka2 += 2){
+    if( angka2 % 3 === 0 ){
+        console.log(angka2 + ' kelipatan 3')
+        } else {
+            console.log(angka2);
+            
+        }
+    }
+
+for(let angka3 = 1; angka3 <= 100; angka3 += 5){
+    if( angka3 % 6 === 0){
+        console.log(angka3 + ' kelipatan 6')
+    } else {
+        console.log( angka3);
+    }
+}
+
+for(let angka4 = 1; angka4 <= 100; angka4 += 9){
+    if( angka4 % 10 === 0){
+        console.log(angka4 + ' kelipatan 10')
+    } else {
+        console.log(angka4);
+    }
+}
